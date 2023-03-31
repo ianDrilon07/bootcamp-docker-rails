1f1795b5557df76e8b99e20f43dfce3993b433d0
Ian Drilon <ian.drilon@mashupgarage.com>
2023-03-31 14:51:24 +0800

    save

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,37 @@
+class UserController < ApplicationController
+  def index
+    @user = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to "user#index"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show 
+    @user = Dog.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(user).permit(:firstname, :lastname, :username, :password)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+    validates :firstname, presence: true
+    validates :lastname, presence: true
+    validates :username, presence: true
+    validates :password, presence: true
+
+    def user 
+        create_table :users do |t|
+            t.string :firstname
+            t.string :lastname
+            t.string :username
+            t.string :password
+        end
+    end
 end
